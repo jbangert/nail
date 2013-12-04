0f8db0a93ef94f4e3e97ea8d98756728943edc81
Julian Bangert <jbangert@acm.org>
2013-12-04 17:56:24 -0500

    doo

--- examples/trivial/grammar.h
+++ examples/trivial/grammar.h
@@ -1,7 +1,8 @@
 #include <hammer/macros.h>
 GRAMMAR_BEGIN(foo)
 #define H_NAME foo
-H_STRUCT_SEQ( H_TO_SINT(int,name1,h_int32()) H_TO_SINT(int,name2,h_int32()))
+H_STRUCT_SEQ( H_TO_SINT(int,name1,h_int32()) 
+              H_TO_SINT(int,name2,h_int32()))
 #undef H_NAME
 GRAMMAR_END(foo)
 #include <hammer/macros_end.h>
