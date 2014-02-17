3ecdfa1e81bafa3038878d4a0ddbce5246e79ea3
Julian Bangert <jbangert@acm.org>
2014-02-16 22:46:46 -0500

    ugly ugly ugly

--- nail/macros/int-struct-decl.h
+++ nail/macros/int-struct-decl.h
@@ -5,7 +5,7 @@
 #define NX_LENGTHVALUE_HACK(lengthp,elemp)
 #define N_STRUCT(inner) 
 #define N_OPTIONAL(inner)
-#define N_DEFPARSER(name,inner) struct name; typedef struct name name; HParser *hammer_##name(); static HParser *hammer_x_##name(); void print_ ## name(const struct name *val, FILE *out,int indent); int gen_##name(HBitWriter *out,struct name *val); extern  struct  name * parse_## name(const uint8_t* input, size_t length);
+#define N_DEFPARSER(name,inner) struct name; typedef struct name name; HParser *hammer_##name(); static HParser *hammer_x_##name(); void print_ ## name(const struct name *val, FILE *out,int indent); int gen_##name(HBitWriter *out,struct name *val); extern  struct  name * parse_## name(const uint8_t* input, size_t length); static void bind_##name (const HParseResult *p,const HParsedToken *val, name *out);
 #define N_PARSER(name)
 #define N_REF(name) *
 #define N_CHOICE(inner) 
