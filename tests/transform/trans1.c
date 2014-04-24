f6664e91cc54ee88482013231ecc8f7d8eedb29c
Julian Bangert <jbangert@acm.org>
2014-04-24 18:13:35 -0400

    revert stream aliasing

--- tests/transform/trans1.c
+++ tests/transform/trans1.c
@@ -1,17 +1,13 @@
 //Note that all nail offsets are in bits
-int offsetsize_parse(NailArena *tmp,NailStream **str_filestream,NailStream *contentstream,uint32_t* offset,uint32_t* filesize){
+int offsetsize_parse(NailArena *tmp,NailStream *str_filestream,NailStream *contentstream,uint32_t* offset,uint32_t* filesize){
         if(*offset > contentstream->size)
                 return -1;
         if(*filesize > contentstream->size  - *offset)
                 return -1;
-        NailStream *filestream = NailStream_alloc(tmp);
-        if(!filestream) 
-                return -1;
-        *str_filestream = filestream;
-        filestream->data = contentstream->data + *offset;
-        filestream->pos  = 0;
-        filestream->size = *filesize;
-        filestream->bit_offset = 0;
+        str_filestream->data = contentstream->data + *offset;
+        str_filestream->pos  = 0;
+        str_filestream->size = *filesize;
+        str_filestream->bit_offset = 0;
         return 0;
 }
 int offsetsize_generate(NailArena *tmp, NailStream *str_filestream, NailStream *contentstream, uint32_t *offset, uint32_t *filesize){
@@ -27,22 +23,18 @@ int offsetsize_generate(NailArena *tmp, NailStream *str_filestream, NailStream *
         return 0;
 }
 static const int headersize = 1024;
-int splitzip_parse(NailArena *tmp,NailStream **str_header,NailStream **str_content,NailStream *current){
+int splitzip_parse(NailArena *tmp,NailStream *str_header,NailStream *str_content,NailStream *current){
         if(current->size <= headersize)
                 return -1;
         if(current->pos != 0)
                 return -1;
-        if(!(str_header[0] = NailStream_alloc(tmp)))
-           return -1;
-        str_header[0]->data = current->data + current->size - headersize;
-        str_header[0]->pos = 0;
-        str_header[0]->size = headersize;
-        str_header[0]->bit_offset = 0;
-        if(!(str_content[0] = NailStream_alloc(tmp)))
-           return -1;
-        str_content[0]->data = current->data;
-        str_content[0]->pos = 0;
-        str_content[0]->size = current->size - headersize;
+        str_header->data = current->data + current->size - headersize;
+        str_header->pos = 0;
+        str_header->size = headersize;
+        str_header->bit_offset = 0;
+        str_content->data = current->data;
+        str_content->pos = 0;
+        str_content->size = current->size - headersize;
         current->pos = current->size; //We consumed all of current. 
         return 0;
 }
