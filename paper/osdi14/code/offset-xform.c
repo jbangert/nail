5e30c385004132fa599e8f142dd538bdb6924bd4
Nickolai Zeldovich <nickolai@csail.mit.edu>
2014-04-30 22:50:21 -0400

    x

--- paper/osdi14/code/offset-xform.c
+++ paper/osdi14/code/offset-xform.c
@@ -5,7 +5,7 @@ offset_u32_parse(NailArena *tmp,
                  const uint32_t *off)
 {
   /* out_str = suffix of in_current
-               at offset off */
+               at offset *off */
 }
 
 int
@@ -14,6 +14,6 @@ offset_u32_generate(NailArena *tmp,
                     NailStream *out_current,
                     uint32_t *off)
 {
-  /* off = position of out_current */
+  /* *off = position of out_current */
   /* append in_fragment to out_current */
 }
