69f8d878900b4cbda1d0aaa4fc6bc1bc279431d9
Julian Bangert <jbangert@acm.org>
2014-05-01 22:02:27 -0400

    Shortened code

--- paper/osdi14/code/dns-xform.c
+++ paper/osdi14/code/dns-xform.c
@@ -1,10 +1,9 @@
-int
-dnscompress_parse(NailArena *tmp,
+
+int dnscompress_parse(NailArena *tmp,
                   NailStream *out_decomp,
                   NailStream *in_current);
 
-int
-dnscompress_generate(NailArena *tmp,
+int dnscompress_generate(NailArena *tmp,
                      NailStream *in_decomp,
                      NailStream *out_current);
 
--- paper/osdi14/code/offset-xform.c
+++ paper/osdi14/code/offset-xform.c
@@ -1,19 +1,15 @@
-int
-offset_u32_parse(NailArena *tmp,
+int offset_u32_parse(NailArena *tmp,
                  NailStream *out_str,
                  NailStream *in_current,
-                 const uint32_t *off)
-{
+                 const uint32_t *off){
   /* out_str = suffix of in_current
                at offset *off */
 }
 
-int
-offset_u32_generate(NailArena *tmp,
+int offset_u32_generate(NailArena *tmp,
                     NailStream *in_fragment,
                     NailStream *out_current,
-                    uint32_t *off)
-{
+                    uint32_t *off){
   /* *off = position of out_current */
   /* append in_fragment to out_current */
 }
