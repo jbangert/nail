453b6741924fab054a94939cb997ca4f27816bfc
Nickolai Zeldovich <nickolai@csail.mit.edu>
2014-09-10 18:56:40 -0400

    tweak DNS grammar figure

--- paper/osdi14/code/dns-api.c
+++ paper/osdi14/code/dns-api.c
@@ -1,17 +1,7 @@
-struct dnspacket {
-    uint16_t id;
-    uint8_t qr;
-    /*  abbreviated */
-    struct {
-        question*elem;
-        size_t count;
-    } questions;
-}; 
-/* Other structures */
-struct dnspacket*parse_dnspacket(
-  NailArena *arena, 
-  const uint8_t *data, 
+struct dnspacket *parse_dnspacket(NailArena *arena,
+  const uint8_t *data,
   size_t size);
+
 int gen_dnspacket(NailArena *tmp_arena,
   NailStream *out,
-  struct dnspacket * val) ;
+  struct dnspacket *val);
