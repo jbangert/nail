35d0f0225abacff80461a7646f20e20b7b64a8e4
Julian Bangert <jbangert@acm.org>
2014-09-10 17:28:35 -0400

    New figure

--- paper/osdi14/code/dns-api.c
+++ paper/osdi14/code/dns-api.c
@@ -1,4 +1,15 @@
-struct dnspacket*parse_dnspacket(NailArena *arena, 
+struct dnspacket {
+    uint16_t id;
+    uint8_t qr;
+    /*  abbreviated */
+    struct {
+        question*elem;
+        size_t count;
+    } questions;
+}; 
+/* Other structures */
+struct dnspacket*parse_dnspacket(
+  NailArena *arena, 
   const uint8_t *data, 
   size_t size);
 int gen_dnspacket(NailArena *tmp_arena,
