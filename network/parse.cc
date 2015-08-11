a3b651f9c075cf89014a3f3524904ce91e7bf06f
Julian Bangert <jbangert@acm.org>
2015-08-11 16:46:46 -0400

    working ip header checksum, now in clean

--- network/parse.cc
+++ network/parse.cc
@@ -1,5 +1,6 @@
 #include "parse.hh"
 #include <nail/size.hh>
+#include <arpa/inet.h> //htons
 /* template<typename siz_t> struct NailCoroFragmentStream {
 private:
   typedef std::map<siz_t, NailMemStream> inner_t;
@@ -10,14 +11,10 @@ public:
     NailMemStream::pos_t pos;
   } 
   };*/
-template <typename str> struct ip_checksum_parse {
+template <typename str> struct internet_checksum_parse {
 };
-template <> struct ip_checksum_parse<NailMemStream> {
-  typedef NailMemStream out_1_t;
-  typedef NailMemStream out_2_t;
-  typedef NailMemStream out_3_t;
-  static int f(NailArena *tmp,NailMemStream *in_current,uint16_t *ptr_checksum){
-       
+template <> struct internet_checksum_parse<NailMemStream> {
+  static int f(NailArena *tmp,NailMemStream *in_current,uint16_t *ptr_checksum){       
     uint32_t checksum = 0;
     uint16_t *field = (uint16_t *)in_current->getBuf();
     for(size_t l = in_current->getSize()/2; l>0;l--){
@@ -31,9 +28,9 @@ template <> struct ip_checksum_parse<NailMemStream> {
     return 0;
   }
 };
-int ip_checksum_generate(NailArena *tmp, NailOutStream *current, uint16_t *ptr_checksum, uint16_t extra_1=0, uint16_t extra_2 =0){
-  
-  uint32_t checksum=extra_1+extra_2;
+
+int internet_checksum_generate(NailArena *tmp, NailOutStream *current, uint16_t *ptr_checksum){
+  uint32_t checksum=0;
   const uint16_t *field =  (uint16_t *)current->data;
   for(size_t ihl = current->pos/2; ihl>0;ihl--){
       checksum += *field;
@@ -42,28 +39,45 @@ int ip_checksum_generate(NailArena *tmp, NailOutStream *current, uint16_t *ptr_c
   while(checksum >> 16){
     checksum = (checksum & 0xFFFF)+ (checksum>>16);
   }
-  *ptr_checksum = ~((uint16_t)checksum);
+  *ptr_checksum = htons(~((uint16_t)checksum));
   return 0;
 }
+template <typename str> struct ip_header_checksum_parse {
+};
+template <> struct ip_header_checksum_parse<NailMemStream> {
+  static int f(NailArena *tmp,NailMemStream *in_current,uint16_t *ptr_checksum, uint8_t *ihl){
+    if(*ihl* 4 > in_current->getSize()) return -1;
+    NailMemStream total_header(in_current->getBuf(), *ihl*4);
+    return internet_checksum_parse<NailMemStream>::f(tmp,&total_header, ptr_checksum);        
+  }
+};
+
+int ip_header_checksum_generate(NailArena *tmp, NailOutStream *current, uint16_t *ptr_checksum, uint8_t *ihl){
+  if(*ihl* 4 > current->size) return -1;
+  NailOutStream total_header = {.data = current->data,
+                                .size = (size_t)*ihl*4,
+                                .pos = (size_t) *ihl*4,
+                                .bit_offset = 0};
+  return internet_checksum_generate(tmp,&total_header, ptr_checksum);        
+}
+
 template <typename str> struct ip_header_parse {
 };
 template <> struct ip_header_parse<NailMemStream> {
   typedef NailMemStream out_1_t;
   typedef NailMemStream out_2_t;
   typedef NailMemStream out_3_t;
-  static int f(NailArena *tmp, NailMemStream **out_options,NailMemStream **out_body,NailMemStream *in_current, uint8_t *ihl, uint16_t *ptr_checksum,uint16_t *total_length){
+  static int f(NailArena *tmp, NailMemStream **out_options,NailMemStream **out_body,NailMemStream *in_current, uint8_t *ihl, uint16_t *total_length,uint8_t *protocol){
     if(n_fail(in_current->repositionOffset(*ihl * 4, 0))) return -1;
     if(in_current->getSize() != *total_length) return -1;
     *out_options = (NailMemStream *)n_malloc(tmp,sizeof(NailMemStream));
     new((void *)*out_options) NailMemStream(in_current->getBuf() + 20, *ihl * 4 - 20);
-    NailMemStream total_header(in_current->getBuf(), *ihl*4);
-    if(n_fail(ip_checksum_parse<NailMemStream>::f(tmp,&total_header, ptr_checksum))) return -1;    
     *out_body = (NailMemStream *)n_malloc(tmp,sizeof(NailMemStream));
     new((void *)*out_body) NailMemStream(in_current->getBuf() +  *ihl * 4, in_current->getSize()- (*ihl * 4));
     return 0;
   }
 };
-int ip_header_generate(NailArena *tmp,  const NailOutStream* in_options, const NailOutStream *in_body, NailOutStream *current, uint8_t *ihl, uint16_t *out_checksum,uint16_t *total_length)
+int ip_header_generate(NailArena *tmp,  const NailOutStream* in_options, const NailOutStream *in_body, NailOutStream *current, uint8_t *ihl,uint16_t *total_length, uint8_t *protocol)
 {
   if(in_options->pos % 4 != 0) return -1; // XXX:: HEADER PADDING!
   if(in_options->pos + 20 >= 64) return -1; // XXX: option length
@@ -75,7 +89,6 @@ int ip_header_generate(NailArena *tmp,  const NailOutStream* in_options, const N
   if(n_fail(tail_generate(tmp, in_body, current))) return -1;
   *total_length = *ihl * 4 + in_body->pos;
   
-  ip_checksum_generate(tmp, current, out_checksum, *ihl, *total_length);
   return 0;
 }
   
--- network/tunclient.cc
+++ network/tunclient.cc
@@ -7,6 +7,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <arpa/inet.h>
 #include <linux/if.h>
 #include <linux/if_tun.h>
 #include <time.h>
